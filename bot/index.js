4179dbfd0b422e32dc2eb703e94150730a1d3bb3
disgustment <regoregitating@gmail.com>
2026-03-29 18:44:30 -0700

    Fix welcome message formatting for new members
    Corrected the greeting message for new members.

--- bot/index.js
+++ bot/index.js
@@ -477,7 +477,7 @@ function buildWelcomeEmbed(member) {
     .setTitle('**Welcome to Limb!**')
     .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
     .setDescription(
-      `hey **${member.user.username}**, welcome to **Limb** \n\n` +
+      `Hey **${member.user.username}**, welcome to **Limb** \n\n` +
       `> 🎫 support: <#${TICKET_PANEL_CHANNEL}>\n` +
       `> 📜 rules: <#${RULES_CHANNEL}>\n\n` +
       'Enjoy your stay at Limb, have fun, and make yourself at home!'
